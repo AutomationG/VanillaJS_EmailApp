99a13af8f90383fef4607a8df35df752b57e0617
AutomationG <pareek23gargi@gmail.com>
2020-07-03 02:03:00 +0530

    1 file modified

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
-// Import stylesheets
 import './style.css';
 
-// Write Javascript code!
 const appDiv = document.getElementById('app');
+
+class Email{
+
+  constructor(){
+   this.data = [{name:'LinkedIn', emailID : 'abc@linkedIn.com', title='Linked In Jobs', content :'sdjbjbcjcbjcajscn'},
+   {name:'Myntra', emailID : 'abc@myntra.com', title='Myntra', content :'sdjbjbcjcbjcajscn'},
+   {name:'Medium', emailID : 'abc@medium.com', title='Javascript', content :'sdjbjbcjcbjcajscn'}]
+  }
+}
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
