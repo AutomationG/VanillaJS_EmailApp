d5cca48a9d2cb3edf2f5e7feac38f33b73209609
AutomationG <pareek23gargi@gmail.com>
2020-07-05 12:23:17 +0530

    2 files modified

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 import './style.css';
 
 function initialData(){
-   return [{name:'LinkedIn', emailID : 'abc@linkedIn.com', title:'Linked In Jobs',  sub :'sdjb jbcjc bjc ajscn'},
-   {name:'Myntra', emailID : 'abc@myntra.com', title:'Myntra', sub :'sdjbj bc jcb jcajscn'},
-   {name:'Medium', emailID : 'abc@medium.com', title:'Javascript', sub :'sdj bj bcj cbj caj scn'}]
+   return [{name:'LinkedIn', emailID : 'abc@linkedIn.com', title:'Linked In Jobs', 
+   sub :'sdjb jbcjc bjc ajscn'},{name:'Myntra', emailID : 'abc@myntra.com', title:'Myntra',
+   sub :'sdjbj bc jcb jcajscn'},{name:'Medium', emailID : 'abc@medium.com', title:'Javascript', sub :'sdj bj bcj cbj caj scn'}]
   }
 
   function createTable(){
@@ -19,20 +19,7 @@ function initialData(){
      view = view + "</table>";
      document.getElementById('data').innerHTML = view;
   }
+ 
   createTable();
-
-  function openTab(index){
-   let newWin = window.open()
-   let data = initialData()
-   let content = '<html>'+
-                  '<body>' +
-                  '<div>'+data[index]['title']+'</div>' +
-                  '<div>'+data[index]['emailID']+'</div>' +
-                  '<div>'+data[index]['sub']+'</div>' +
-                  '</body>' +
-                  '</html>' +
-   newWin.document.write(content)
-
-  }
 
   
