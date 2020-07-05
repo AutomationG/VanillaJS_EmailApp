7f4114f16b96e88d40b89de49ae89a2d80dbf5e0
AutomationG <pareek23gargi@gmail.com>
2020-07-05 11:23:35 +0530

    2 files modified

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
 import './style.css';
 
-class Email{
+  <table>
+  <tr onclick='openTab(0)'>
+    <td class='underline'>LinkedIn</td>
+    <td>hdjj sjnsjnj asn</td>
+  </tr>
+   <tr>
+    <td class='underline'>LinkedIn</td>
+    <td>hdj jsjns jnjasn</td>
+  </tr>
+  <tr>
+    <td class='underline'>LinkedIn</td>
+    <td>hdj jsjns jnjasn</td>
+  </tr>
+</table>
 
-  constructor(){
-   this.data = [{name:'LinkedIn', emailID : 'abc@linkedIn.com', title='Linked In Jobs', content :'sdjbjbcjcbjcajscn'},
+  function initialData(){
+   return [{name:'LinkedIn', emailID : 'abc@linkedIn.com', title='Linked In Jobs', content :'sdjbjbcjcbjcajscn'},
    {name:'Myntra', emailID : 'abc@myntra.com', title='Myntra', content :'sdjbjbcjcbjcajscn'},
    {name:'Medium', emailID : 'abc@medium.com', title='Javascript', content :'sdjbjbcjcbjcajscn'}]
   }
-}
+
+  function openTab(){
+
+   let newWin = window.open()
+   let data = initialData()
+   let content = '<html>'+
+                  '<body>' +
+                  '<div></div>' +
+                  '<div></div>' +
+                  '<div></div>' +
+                  '</body>' +
+                  '</html>' +
+   newWin.write()
+
+  }
+
+  
