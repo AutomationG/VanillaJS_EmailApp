310f44b9402a2cc79ad475d17758926bb1fa3b31
AutomationG <pareek23gargi@gmail.com>
2020-07-05 11:43:59 +0530

    2 files modified

--- index.js
+++ index.js
@@ -1,35 +1,34 @@
 import './style.css';
 
-  <table>
-  <tr onclick='openTab(0)'>
-    <td class='underline'>LinkedIn</td>
-    <td>hdjj sjnsjnj asn</td>
-  </tr>
-   <tr>
-    <td class='underline'>LinkedIn</td>
-    <td>hdj jsjns jnjasn</td>
-  </tr>
-  <tr>
-    <td class='underline'>LinkedIn</td>
-    <td>hdj jsjns jnjasn</td>
-  </tr>
-</table>
-
-  function initialData(){
-   return [{name:'LinkedIn', emailID : 'abc@linkedIn.com', title='Linked In Jobs', content :'sdjbjbcjcbjcajscn'},
-   {name:'Myntra', emailID : 'abc@myntra.com', title='Myntra', content :'sdjbjbcjcbjcajscn'},
-   {name:'Medium', emailID : 'abc@medium.com', title='Javascript', content :'sdjbjbcjcbjcajscn'}]
+function initialData(){
+   return [{name:'LinkedIn', emailID : 'abc@linkedIn.com', title:'Linked In Jobs',  sub :'sdjb jbcjc bjc ajscn'},
+   {name:'Myntra', emailID : 'abc@myntra.com', title:'Myntra', sub :'sdjbj bc jcb jcajscn'},
+   {name:'Medium', emailID : 'abc@medium.com', title:'Javascript', sub :'sdj bj bcj cbj caj scn'}]
   }
 
-  function openTab(){
-
+  function createTable(){
+    let content = initialData()
+    let view= "<table>";
+    for(let i=0; i< content.length;i++){
+    view =  view +
+            "<tr onclick='openTab(i)'>" + 
+            "<td class='underline'>"+content[i]['name']+"</td>"+
+            "<td>" + content[i]['sub']+"</td>"+
+            "</tr>"
+    }
+     view = view + "</table>";
+     document.getElementById('data').innerHTML = view;
+  }
+  createTable();
+  
+  function openTab(index){
    let newWin = window.open()
    let data = initialData()
    let content = '<html>'+
                   '<body>' +
-                  '<div></div>' +
-                  '<div></div>' +
-                  '<div></div>' +
+                  '<div>'+content[index]['title']+'</div>' +
+                  '<div>'+content[index]['emailID']+'</div>' +
+                  '<div>'+content[index]['sub']+'</div>' +
                   '</body>' +
                   '</html>' +
    newWin.write()
